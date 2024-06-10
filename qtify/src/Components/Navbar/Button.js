81523d959f25827bd6397bdaa3e9e322f92b0bcd
Mohammed Faizan Ghani <mohammedfaizanghani78@gmail.com>
2024-06-10 17:11:59 +0530

    button create

---
+++ qtify/src/Components/Navbar/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Button({Child}) {
+  return (
+    <button>{Child}</button>
+  )
+}
+
+export default Button
---
+++ qtify/src/Components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import styles from "./Navbar.module.css"
+import Button from './Button'
+
+function Navbar() {
+  return (
+    <>
+    <div className={styles.nav}>
+    
+    <Button/>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
