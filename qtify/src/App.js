fb8c1829bf3078533389e6fb767b720bebbe112c
Mohammed Faizan Ghani <mohammedfaizanghani78@gmail.com>
2024-06-10 17:15:19 +0530

    Calling Navbar in App.js

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <>
-    <h1>Hi</h1>
+    <Navbar/>
     </>
   );
 }
